8ff19edb8237663e6ca207924b32609c58a0ce17
msilis <mikssilis@gmail.com>
2024-08-15 20:21:09 +0100

    add button events

---
+++ script.js
@@ -0,0 +1,44 @@
+addEventListener("DOMContentLoaded", () => {
+  const playButton = document.getElementById("playButton");
+  const stopButton = document.getElementById("stopButton");
+  const leftSpool = document.getElementById("leftSpoolInner");
+  const rightSpool = document.getElementById("rightSpoolInner");
+
+  const getCssRules = (selector) => {
+    const allRules = document.styleSheets;
+
+    for (const sheet of allRules) {
+      try {
+        const rules = sheet.cssRules;
+        const rule = [...rules].find((rule) => rule.selectorText === selector);
+        if (rule) return rule;
+      } catch (error) {
+        console.error(
+          (`Error accessing CSS rule for selector '${selector}':`, error),
+        );
+      }
+      return null;
+    }
+  };
+
+  const stylesheet = document.styleSheets;
+
+  const handlePlayClick = () => {
+    leftSpool.style.setProperty(
+      "animation",
+      "cassettePlayAnimation 2s linear infinite",
+    );
+    rightSpool.style.setProperty(
+      "animation",
+      "cassettePlayAnimation 2s linear infinite",
+    );
+  };
+
+  const handleStopClick = () => {
+    leftSpool.style.removeProperty("animation");
+    rightSpool.style.removeProperty("animation");
+  };
+
+  playButton.addEventListener("click", handlePlayClick);
+  stopButton.addEventListener("click", handleStopClick);
+});
